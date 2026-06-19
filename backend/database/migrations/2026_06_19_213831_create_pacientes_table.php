f431f4e8cac495ce8c8552a7efb2c26849b083d7
Flavio Salgado <flaviosalgado2@hotmail.com>
2026-06-19 19:32:26 -0300

    feat: backend - Migrations de banco para Pacientes.

---
+++ backend/database/migrations/2026_06_19_213831_create_pacientes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->date('data_nascimento');
+            $table->string('cpf', 11)->unique();
+            $table->string('sexo', 1);
+            $table->string('cep', 8);
+            $table->string('cidade');
+            $table->string('endereco');
+            $table->string('complemento')->nullable();
+            $table->string('status', 10)->default('ativo');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
